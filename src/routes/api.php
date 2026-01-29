3be18ddbc4b1fdfa81da805cf289d11a3eecd91a
iromerovazquez13-wq <iromerovazquez13@gmail.com>
2026-01-29 20:01:34 +0000

    Etapa 2

---
+++ src/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::post('/eventos', [EventoController::class, 'store']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+Route::put('/eventos/{id}', [EventoController::class, 'update']);
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
